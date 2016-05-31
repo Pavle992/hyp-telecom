ccf07f1e95783fe78bc36bfce89b81db57a640c8
kikice010 <kikice010@gmail.com>
2016-05-31 23:17:55 +0200

    Insert script for mobile devices

---
+++ sql scripts/insert_phones.sql
@@ -0,0 +1,55 @@
+INSERT INTO DEVICE(name,price,description,cpu,os,resolution,display_size,display_type,sim,weight,dimensions,device_type) VALUES 
+
+('Huawei p9',600,'Huawei P9 in its design lines with a strong visual impact, characterized by diamond-cut edges and rounded to perfection. The smartphone has a unibody aerospace aluminum and glass upgraded 2.5D, and is available in two colors: Titanium Grey and Mystic Silver.
+
+With a 96% color gamut, stunning Full HD IPS display, 5.2 "allows you to discover a world full of intense colors and more natural.
+
+The cooperation between Huawei and Leica has created a smartphone with dual camera. The result: more photo and video sensational brightness and sharpness.
+
+Huawei P9 is capable of capturing images with bright colors while ensuring a yield extraordinary black and white; all with the timeless style of Leica.La technology with dual Huawei P9 camera allows you to capture a larger amount of light, combining the best qualities of color acquired from RGB sensor with details obtained from the sensor in black and white. All to ensure maximum results with every shot.
+','Quad-core 2.5 GHz Cortex-A72 & quad-core 1.8 GHz Cortex-A53','Android OS, v6.0 (Marshmallow)','1080 x 1920 pixels (~423 ppi pixel density)','5.2 inches (~72.9% screen-to-body ratio)','IPS-NEO LCD capacitive touchscreen, 16M colors','Single SIM (Nano-SIM) or Dual SIM (Nano-SIM, dual stand-by)','144 g (5.08 oz)','145 x 70.9 x 7 mm (5.71 x 2.79 x 0.28 in)','Phone'),
+
+('iPhone SE',510,'The phone 4 "more powerful than ever. IPhone SE shooting a compact design highly successful addition of two advanced cameras and a power of incredibile.Disegnato processing to fit comfortably in your hand and be pleasant to the touch, thanks to the satin finish of the aluminum shell microsabbiato.
+iPhone SE has a Retina display, 4 '', archittetura A9 chip with 64-bit desktop-class, fingerprint sensor Touch ID, 12MP iSight camera, FaceTime HD camera with flash Retina, Live Photos and Wi-Fi connections and 4G fast. And yet, iOS 9 and iCloud.','Dual-core 1.84 GHz Twister','iOS 9.3.2','640 x 1136 pixels (~326 ppi pixel density)','4.0 inches (~60.8% screen-to-body ratio)','LED-backlit IPS LCD, capacitive touchscreen, 16M colors','Nano-SIM','113 g (3.99 oz)','123.8 x 58.6 x 7.6 mm (4.87 x 2.31 x 0.30 in)','Phone'),
+
+('Iphone 6s',600,'Retina HD display, 4.7 "3D Touch. 7000 series aluminum and more resistant glass. A9 chip with 64-bit architecture of desktop level. New iSight camera with 12MP Live Photos. Touch ID. Wi-Fi connections and 4G LTE more veloci.1 long battery durata.2 and yet, iOS 9 and iCloud. all in a sleek unibody.','Dual-core 1.84 GHz Twister','iOS 9, upgradable to iOS 9.3.2','750 x 1334 pixels (~326 ppi pixel density)','4.7 inches (~65.6% screen-to-body ratio)','LED-backlit IPS LCD, capacitive touchscreen, 16M colors','Nano-SIM','143 g (5.04 oz)','138.3 x 67.1 x 7.1 mm (5.44 x 2.64 x 0.28 in)','Phone'),
+
+('LG G5',700,'The new LG G5 is a new type of smartphone that exceeds all limits, with a modular design that revolutionizes the metal fully specified design standards. Expand the functions, interact with the innovative LG Friends and test truly innovative smartphone experience. The G5 revolution continues with a double wide-angle camera, which captures more than your eyes can see. Even under the sun, because the new Quantum IPS display is richer, bright and colorful even in direct sunlight. And it always shows the main notifications even when not active. New LG G5: what you do not expect, in the palm of your hand.
+Beauty and sophistication into the palm of your hand. Looking for more attention to detail and elegance of the materials, and here they are. The metal and glass come together softly in a simply iconic shape. That gives you all sensations to try.
+Freed from the conventions. The new modular design with battery slide will allow you to have all that others have never been able to give. The freedom to have a metal smartphone and they can change the battery when you want. And to expand the functions with new modules.','Dual-core 2.15 GHz Kryo & dual-core 1.6 GHz Kryo','Android OS, v6.0.1 (Marshmallow)','1440 x 2560 pixels (~554 ppi pixel density)','5.3 inches (~70.1% screen-to-body ratio)','IPS LCD capacitive touchscreen, 16M colors','Single SIM (Nano-SIM) or Dual SIM (Nano-SIM, dual stand-by)','159 g (5.61 oz)','149.4 x 73.9 x 7.7 mm (5.88 x 2.91 x 0.30 in)','Phone'
+),
+
+('LG K8',180,'LGs new K Series offers a range of smartphones that reflect your personality. LG K8 accompanies you on your travels and will surprise you with detailed selfie to share with all the speed of 4G LTE network
+Give yourself a tone! LG K8 has a design that leaves gaping through beveled glass profile and rounded shape. More is more ergonomic and provides a better grip thanks to the work of the back cover.
+Catch the funniest situations of the day with 8-megapixel camera. It just a touch on the display and K8 focus and shoot immediately. Even for selfies you can count on the 5 megapixel front camera with the virtual flash feature that illuminates your face, so your selfies will be illuminated as they should, while everyone else will snap ... dark selfies!','Dual-core 2.15 GHz Kryo & dual-core 1.6 GHz Kryo','Android OS, v6.0.1 (Marshmallow)','720 x 1280 pixels (~294 ppi pixel density)','5.0 inches (~66.7% screen-to-body ratio)','IPS LCD capacitive touchscreen, 16M colors','Micro-SIM','157 g (5.54 oz)','144.6 x 71.5 x 8.7 mm (5.69 x 2.81 x 0.34 in)','Phone'
+),
+
+('Sony XPERIA X',630,'A smartphone with inimitable performance that integrates a stunning camera that can predict the next movement of the subject ..
+Xperia X Performance integrates the new camera Sony signed, faster and smarter than ever. Faster response, focus more intelligent and revolutionary adaptive shutter speed: youll have everything you need for incredibly sharp shots in all conditions. Making intuitive hybrid automatic Xperia X Performance Fire uses technologies of Sony cameras and allows you to capture any movement. Just choose the subject you want in focus and the camera will automatically follow, even when moving. The focus is always clear and always sharp subject: no need to worry of following the scene. Think of all your smartphone.
+Our rapid camera is able to switch from standby to shoot in less than 0.6 seconds. Thanks to the incredible speed startup, autofocus hybrid focus and image processing, you can capture the most unexpected moments.','Dual-core 2.15 GHz Kryo & dual-core 1.6 GHz Kryo','Android OS, v6.0.1 (Marshmallow)','1080 x 1920 pixels (~441 ppi pixel density)','5.0 inches (~69.6% screen-to-body ratio)','IPS LCD capacitive touchscreen, 16M colors','Single SIM (Nano-SIM) or Dual SIM (Nano-SIM, dual stand-by)','153 g (5.40 oz)','142.7 x 69.4 x 7.9 mm (5.62 x 2.73 x 0.31 in)','Phone'
+),
+
+('ZTE Blade L5',80,'The phone comes with a 5.00-inch display with a resolution of 720 pixels by 1280 pixels 
+
+The ZTE Blade L5 Plus is powered by quad-core MediaTek MT6580 processor and it comes with 1GB of RAM. The phone packs 8GB of internal storage that can be expanded via a microSD card. As far as the cameras are concerned, the ZTE Blade L5 Plus packs a 8-megapixel primary camera on the rear and a 2-megapixel front shooter for selfies. 
+
+The ZTE Blade L5 Plus runs Android 5.1 and is powered by a 2150mAh non removable battery. It measures 143.00 x 72.00 x 8.30 (height x width x thickness) and weighs 160.00 grams.','Quad-core 1.3 GHz Cortex-A7','Android OS, v5.1 (Lollipop)','720 x 1280 pixels (~294 ppi pixel density)','5.0 inches (~69.6% screen-to-body ratio)','Capacitive touchscreen, 16M colors','Dual SIM','160 g (5.64 oz)','143 x 72 x 8.3 mm (5.63 x 2.83 x 0.33 in)','Phone'
+),
+
+('LG Nexus 5X',450,'Nexus 5X captures all the spirit of the Nexus family, offering high-level performance in a compact, lightweight device ready to accompany you throughout the day. With the display of 5.2 '' and the processor Hexa-Core Qualcomm Snapdragon 808 processor will have a maximum of visual experience.
+The Nexus 5X camera is the best that has ever been mounted on a Nexus smartphone. His special is that it takes quality pictures even in less than ideal lighting conditions. The 1.55μm pixel capture more details even when there is very little light to give you sharp pictures with stunning detail. And with the Google apps even quicker to boot Camera and take pictures, youll never miss a moment of your life.
+Google Photo is the new way to save, search and share all your photos and videos. Save an unlimited free cloud-quality photos and high quality video, which you can access from anywhere. You can search your content depending on the person, place, or the subject you have photographed, thanks to the organization of intelligent Google Photos. So you can go back in time and revisit your memories with more ease.','Quad-core 1.44 GHz Cortex-A53 & dual-core 1.82 GHz Cortex-A57','Android OS, v6.0 (Marshmallow)','1080 x 1920 pixels (~423 ppi pixel density)','5.2 inches (~70.2% screen-to-body ratio)','IPS LCD capacitive touchscreen, 16M colors','Nano-SIM','136 g (4.80 oz)','147 x 72.6 x 7.9 mm (5.79 x 2.86 x 0.31 in)','Phone'
+),
+
+('Microsoft Lumia 950',500,'Superior functionality, excellent design and the best experience with Windows 10: choose Lumia 950 and transforms it at any time in a great achievement.
+Learn all you can do with your Microsoft Lumia','Dual-core 1.82 GHz Cortex-A57 & quad-core 1.44 GHz Cortex-A53','Microsoft Windows 10','1440 x 2560 pixels (~564 ppi pixel density)','5.2 inches (~70.5% screen-to-body ratio)','AMOLED capacitive touchscreen, 16M colors','Nano-SIM','150 g (5.29 oz)','145 x 73.2 x 8.2 mm (5.71 x 2.88 x 0.32 in)','Phone'
+),
+
+('Samsung Galaxy S7 edge ',830,'Samsung Galaxy S7 Edge smartphone was launched in February 2016. The phone comes with a 5.50-inch touchscreen display with a resolution of 1440 pixels by 2560 pixels at a PPI of 534 pixels per inch. 
+
+The Samsung Galaxy S7 Edge is powered by 1.6GHz octa-core and it comes with 4GB of RAM. The phone packs 32GB of internal storage that can be expanded up to 200GB via a microSD card. As far as the cameras are concerned, the Samsung Galaxy S7 Edge packs a 12-megapixel primary camera on the rear and a 5-megapixel front shooter for selfies. 
+
+The Samsung Galaxy S7 Edge runs Android 6.0 and is powered by a 3600mAh non removable battery. It measures 150.90 x 72.60 x 7.70 (height x width x thickness) and weighs 157.00 grams.','Dual-core 2.15 GHz Kryo & dual-core 1.6 GHz Kryo, Quad-core 2.3 GHz Mongoose + quad-core 1.6 GHz Cortex-A53','Android OS, v6.0 (Marshmallow)','1440 x 2560 pixels (~534 ppi pixel density)','5.5 inches (~76.1% screen-to-body ratio)','Super AMOLED capacitive touchscreen, 16M colors','Single SIM (Nano-SIM) or Dual SIM (Nano-SIM, dual stand-by)','157 g (5.54 oz)','150.9 x 72.6 x 7.7 mm (5.94 x 2.86 x 0.30 in)','Phone'
+)
+
+
