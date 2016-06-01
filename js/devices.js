5f4b33a8bd8224a2712d24ab6185f41fe381758c
kikice010 <kikice010@gmail.com>
2016-06-02 01:58:40 +0200

    Static version of devices page done - version 1.1

---
+++ js/devices.js
@@ -0,0 +1,16 @@
+
+$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+  var target = $(e.target).attr("href") // activated tab
+  if ($(target).is(':empty')) {
+    $.ajax({
+      type: "GET",
+      url: "/article/",
+      error: function(data){
+        alert("There was a problem");
+      },
+      success: function(data){
+        $(target).html(data);
+      }
+  })
+ }
+})
