2b165cc4315d0d05822c4fe0e89f1edd2a7c1cc6
petarko1990 <petar.korda@gmail.com>
2016-06-06 22:23:01 +0200

    device list page uradjen

---
+++ php/template_landmarks.php
@@ -0,0 +1,40 @@
+
+
+
+    <!-- Fixed navbar -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <span class="nav-brand"><a href="./home.html"><img src="../img/logo.png"/></a></span>
+            </div>
+            <div id="navbar" class="collapse navbar-collapse">
+                
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="./home.html">Home</a>
+                    </li>
+                    <li><a href="./devices.html">Devices</a>
+                    </li>
+                    <li><a href="./smartlife.html">Smart Life</a>
+                    </li>
+                    <li><a href="./assistance.html">Assitance</a>
+                    </li>
+                    <li><a href="./home.html">Who we are</a>
+                    </li>
+                    <li><a href="./home.html">The Group</a>
+                    </li>
+                    <li><a href="./home.html">Contact</a>
+                    </li>
+                    <li><a href="./cart.html">Cart <span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span></a>
+                    </li>
+                </ul>
+            </div>
+            <!--/.nav-collapse -->
+        </div>
+    </nav>
+   
