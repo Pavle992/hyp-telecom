c80d8ede5889b0c54cb6eba25b6d18abc1ca6aa0
petarko1990 <petar.korda@gmail.com>
2016-06-09 20:28:35 +0200

    assistence page done

---
+++ js/assistence.js
@@ -0,0 +1,98 @@
+$(document).ready(function(){
+    
+	getAssistence(1,1,1,0);
+
+});
+
+function handleAllClick(cb) {
+	document.getElementById('filter-all').checked = true;
+	if (document.getElementById('filter-all').checked) {
+		document.getElementById('filter-phone').checked = false;
+		document.getElementById('filter-tablet').checked = false;
+		document.getElementById('filter-smart').checked = false;
+	}
+}
+
+function filterClick(cb) {
+	var getPhone = 0;
+	var getTablet = 0;
+	var getSmart = 0;
+	var prom = 0;
+
+	if (document.getElementById('filter-phone').checked || document.getElementById('filter-tablet').checked || document.getElementById('filter-smart').checked) {
+		document.getElementById('filter-all').checked = false;
+		if (document.getElementById('filter-phone').checked) getPhone = 1;
+		if (document.getElementById('filter-tablet').checked) getTablet = 1;
+		if (document.getElementById('filter-smart').checked) getSmart = 1;
+		
+	}
+	else {
+		document.getElementById('filter-all').checked = true;
+		getPhone = 1;
+		getTablet = 1;
+		getSmart = 1;
+	}
+	
+  	if ($('#prom-tab').attr('class') == "active") prom = 1;
+    getAssistence(getPhone, getTablet, getSmart, prom);
+ 
+}
+
+function handleCatClick() {
+	document.getElementById('filter-all').checked = true;
+	document.getElementById('filter-phone').checked = false;
+	document.getElementById('filter-tablet').checked = false;
+	document.getElementById('filter-smart').checked = false;
+	getAssistence(1,1,1,0);
+}
+
+function handleHighlightClick() {
+	document.getElementById('filter-all').checked = true;
+	document.getElementById('filter-phone').checked = false;
+	document.getElementById('filter-tablet').checked = false;
+	document.getElementById('filter-smart').checked = false;
+	getAssistence(1,1,1,1);
+}
+
+function getAssistence(phone, tablet, smart, prom) {
+	$.ajax({
+      url: '../php/assistence_grid.php',
+      type: 'post',
+      data: {
+      	'phone': phone,
+      	'tablet': tablet,
+      	'smart': smart,
+      	'hilight' : prom
+  			},
+      success: function(data, status) {
+      	 var devices = $.parseJSON(data);
+      	 var size = devices.length;
+      	 var insertString = "";
+
+      	 $.each(devices, function (i, device) {
+      	 	 var insertBox = '<div class="col-lg-3 col-lg-offset-1 device-block"><div class="text-center"><h4>' + device.name + '</h4></div><div class="text-center"><button class="btn btn-primary" type="submit">Details</button></div></div>';
+      	 	 if (i == 0)  {
+      	 	 	insertString+='<div class="row">';
+      	 	 	insertString+=insertBox;
+      	 	 }
+   			 else if (i % 3 == 0 && i != 0) {
+				insertString+='</div>';
+				insertString+='<div class="row">';
+				insertString+=insertBox;
+			 } 
+			 else {
+			 	insertString+=insertBox;
+			 }
+		});
+
+      	insertString+='</div>';
+
+      	$('#grid').html(insertString);
+
+      },
+      error: function(xhr, desc, err) {
+        console.log(xhr);
+        console.log("Details: " + desc + "\nError:" + err);
+      }
+   }); // end ajax call
+}
---
+++ php/assistence_grid.php
@@ -0,0 +1,98 @@
+<?php
+
+require_once('dbconnection.php');
+
+
+
+class AssistenceGrid {
+
+	private $db;
+
+	function __construct() {
+		$this->db = new MySql();
+	}
+
+	function __destruct(){
+		
+	}
+
+	public function getAssistenceByCategory($cat, $hilight = 0) {
+		$this->db->connect();
+
+		if ($hilight) $sql = "SELECT assistance.name FROM assistance WHERE id in (SELECT DISTINCT dev_assistance.id_assistance FROM dev_assistance JOIN device on dev_assistance.id_dev=device.id WHERE device.device_type='$cat') AND highlight_flag = 'Y';";
+		else $sql = "SELECT assistance.name FROM assistance WHERE id in (SELECT DISTINCT dev_assistance.id_assistance FROM dev_assistance JOIN device on dev_assistance.id_dev=device.id WHERE device.device_type='$cat');";
+
+		
+		$result = $this->db->con->query($sql);
+
+		$resultArray = array();
+		$i=0;
+
+		if ($result->num_rows > 0) {
+			
+			
+			while ($row = $result->fetch_assoc()) {
+				$resultArray+=array($i => array('name' => $row['name']));
+				
+				$i+=1;
+			}
+		}
+
+		return $resultArray;	
+
+	}
+
+	public function getAllAssistence($hilight = 0) {
+		$this->db->connect();
+
+		if ($hilight) $sql = "SELECT name  FROM assistance WHERE highlight_flag = 'Y'";
+		else $sql = "SELECT name FROM assistance";
+		$result = $this->db->con->query($sql);
+
+		$resultArray = array();
+		$i=0;
+
+		if ($result->num_rows > 0) {
+			
+			
+			while ($row = $result->fetch_assoc()) {
+				
+				$resultArray+=array($i => array('name' => $row['name']));
+				
+				$i+=1;
+			}
+		}
+
+		return $resultArray;	
+
+	}
+}
+
+
+
+$grid = new AssistenceGrid();
+
+$result = array();
+
+$hilight = $_POST['hilight'];
+
+
+if ($_POST['phone'] == 1 && $_POST['tablet'] == 1 && $_POST['smart'] == 1) {
+	$result =  $grid->getAllAssistence($hilight);
+}
+
+else {
+	if ($_POST['phone'] == 1) $result = array_merge($result, $grid->getAssistenceByCategory("Phone", $hilight));
+	if ($_POST['tablet'] == 1) $result = array_merge($result, $grid->getAssistenceByCategory("Tablet", $hilight));
+	if ($_POST['smart'] == 1) $result = array_merge($result, $grid->getAssistenceByCategory("Smart", $hilight));
+}
+
+$result = array_unique($result, SORT_REGULAR);
+
+
+ echo json_encode($result);
+
+
+
+
+?>
