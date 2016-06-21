6fc39559d03161162ec9569f7bd8277ae3448139
Pavle992 <pavle.vidanovic@gmail.com>
2016-06-21 04:36:00 +0200

    SL_Service Web Service finished

--- php/SmartLife.php
+++ php/SmartLife.php
@@ -1,5 +1,8 @@
 <?php
 
+	include "dbconnection.php";
+	include "Device1.php";
+
 	class SmartLife{
 
 		var $id;
@@ -10,6 +13,8 @@
 		var $category;
 		var $activation_rules;
 
+		var $devices;
+
 		public function SmartLife($json){
 			
 			$this->id=$json["id"];
@@ -19,6 +24,34 @@
 			$this->rates_discount_price=$json["rates_discount_price"];
 			$this->category=$json["category"];
 			$this->activation_rules=$json["activation_rules"];
+			$this->devices=array();
+			
+		}
+
+		public function linkDevices(){
+
+			//taking all linked devices from DB
+
+			$db=new MySql();
+
+			$db->connect();
+
+			$result = mysqli_query($db->con,
+				"select d.*
+				 from dev_sl inner join device d on id_dev=d.id inner join sl_service s on id_sl=s.id
+				 where s.id=$this->id;"
+				 ) or die(mysql_error());
+
+			if(mysqli_num_rows($result) > 0){
+
+				while ($row = mysqli_fetch_array($result)) {
+
+					$entry=new Device1($row);
+					array_push($this->devices, json_encode($entry));
+				}
+			}
+
+
 		}
 	}
 
