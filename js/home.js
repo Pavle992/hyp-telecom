54c222bd78fcd9c4b5e169c116701ac416edfaa1
Pavle992 <pavle.vidanovic@gmail.com>
2016-06-27 17:12:12 +0200

    Pikachu added

--- js/home.js
+++ js/home.js
@@ -64,7 +64,7 @@ $(document).ready(function() {
     // loadContent( url,  rootJsonElement, $containerNode,  function that creates a node to be appended to the $container )
     if (typeof loadContent !== 'undefined') {
         loadContent(
-            'http://hyp-telecom/php/get_homepage_components.php',
+            'http://hyp-telecom.ml/php/get_homepage_components.php',
             'homepage', $('#homepage-container'), loadHomepageComponents);
     }
 
